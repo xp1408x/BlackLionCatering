818c51618e6a0a283e73bdccaba00c8618b3e69a
p1408 <pcortez1408@gmail.com>
2025-06-14 12:32:30 -0500

    change our story

--- components/OurStorySection.tsx
+++ components/OurStorySection.tsx
@@ -8,7 +8,7 @@ const OurStorySection: React.FC = () => {
       className="relative bg-cover bg-center py-20 md:py-32"
       style={{ backgroundImage: "url('https://assets.zyrosite.com/A85VN675bJTvnpRk/our-story-background-AQEZ9NxVgyh29DQ9.webp')" }}
     >
-      <div className="absolute inset-0 bg-[#282828] opacity-60"></div>  Light overlay for text readability
+      <div className="absolute inset-0 bg-[#282828] opacity-60"></div>  
       <div className="container mx-auto px-4 relative z-10 flex justify-center items-center min-h-[500px]">
         <div className="md:w-1/2 p-8 rounded-lg shadow-xl text-center">
           {/* BrandMotif eliminado */}
@@ -17,7 +17,7 @@ const OurStorySection: React.FC = () => {
             UNFORGETTABLE FLAVORS FOR YOUR EVENTS
           </h3>
           <p className="text-white text-[24px] mb-8 leading-relaxed font-roboto">
-            At Black Lion Catering, we believe that food is more than just sustenance; it's an experience, a memory, a way to bring people together. Our philosophy is rooted in creating unforgettable moments that last a lifetime. We are passionate about crafting culinary delights that not only tantalize your taste buds but also tell a story. Each dish is prepared with the freshest ingredients, meticulous attention to detail, and a dash of love, ensuring that your special occasions are graced with flavors that will be treasured and memories that will be cherished.
+            At the heart of every celebration, whether it's the "I do" of a wedding, the vibrant joy of a quinceañera, the camaraderie of a social event, or the sophistication of a corporate meeting, lies a universal desire: to create lasting memories. It was with this deep conviction that Black Lion Catering was born, not just as a food service, but as a promise to transform every event into an unforgettable experience. Black Lion Catering is proud to be the trusted partner for your events. Because in the end, we don't just serve food; we create moments that last, celebrations that resonate, and memories that will be treasured forever. Allow us to be part of your next story.
           </p>
           <div className="text-center">
             <Button variant="outline-white" size="lg" className="text-[34px]">
