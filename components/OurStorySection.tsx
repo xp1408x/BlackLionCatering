b3bc551f3ebaebe822500f6cff150985f15bebc5
p1408 <pcortez1408@gmail.com>
2025-06-14 18:40:39 -0500

    lowercase our story

--- components/OurStorySection.tsx
+++ components/OurStorySection.tsx
@@ -13,8 +13,8 @@ const OurStorySection: React.FC = () => {
         <div className="md:w-1/2 p-8 rounded-lg shadow-xl text-center">
           {/* BrandMotif eliminado */}
           <h2 className="text-[64px] font-bold text-white mb-2">OUR STORY</h2>
-          <h3 className="text-[48px] font-semibold text-white mb-6">
-            UNFORGETTABLE FLAVORS FOR YOUR EVENTS
+          <h3 className="text-[46px] text-white mb-8 normal-case">
+            Unforgettable flavors for your events
           </h3>
           <p className="text-white text-[24px] mb-8 leading-relaxed font-roboto">
             At the heart of every celebration, whether it's the "I do" of a wedding, the vibrant joy of a quinceañera, the camaraderie of a social event, or the sophistication of a corporate meeting, lies a universal desire: to create lasting memories. It was with this deep conviction that Black Lion Catering was born, not just as a food service, but as a promise to transform every event into an unforgettable experience. Black Lion Catering is proud to be the trusted partner for your events. Because in the end, we don't just serve food; we create moments that last, celebrations that resonate, and memories that will be treasured forever. Allow us to be part of your next story.
